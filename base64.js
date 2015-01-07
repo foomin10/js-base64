4c2057b4843c08a138704eeea611e3b1638d8312
foomin10 <foomin10@gmail.com>
2015-01-07 21:50:14 +0900

    cosmetic changes

--- base64.js
+++ base64.js
@@ -14,11 +14,12 @@
     var _Base64 = global.Base64;
     var version = "2.1.6";
     // constants
-    var b64chars
-        = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    var b64chars =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
     var b64tab = function(bin) {
         var t = {};
-        for (var i = 0, l = bin.length; i < l; i++) t[bin.charAt(i)] = i;
+        for (var i=0, l=bin.length; i<l; i++)
+            t[bin.charAt(i)] = i;
         return t;
     }(b64chars);
     var fromCharCode = String.fromCharCode;
@@ -36,10 +37,12 @@
             var cc = 0x10000
                 + (c.charCodeAt(0) - 0xD800) * 0x400
                 + (c.charCodeAt(1) - 0xDC00);
-            return (fromCharCode(0xf0 | ((cc >>> 18) & 0x07))
-                    + fromCharCode(0x80 | ((cc >>> 12) & 0x3f))
-                    + fromCharCode(0x80 | ((cc >>>  6) & 0x3f))
-                    + fromCharCode(0x80 | ( cc         & 0x3f)));
+            return ''
+                + fromCharCode(0xf0 | ((cc >>> 18) & 0x07))
+                + fromCharCode(0x80 | ((cc >>> 12) & 0x3f))
+                + fromCharCode(0x80 | ((cc >>>  6) & 0x3f))
+                + fromCharCode(0x80 | ( cc         & 0x3f))
+            ;
         }
     };
     var re_utob = /[\uD800-\uDBFF][\uDC00-\uDFFFF]|[^\x00-\x7F]/g;
@@ -47,17 +50,16 @@
         return u.replace(re_utob, cb_utob);
     };
     var cb_encode = function(ccc) {
-        var padlen = [0, 2, 1][ccc.length % 3],
-        ord = ccc.charCodeAt(0) << 16
+        var padlen = [0, 2, 1][ccc.length % 3];
+        var ord = ccc.charCodeAt(0) << 16
             | ((ccc.length > 1 ? ccc.charCodeAt(1) : 0) << 8)
-            | ((ccc.length > 2 ? ccc.charCodeAt(2) : 0)),
-        chars = [
-            b64chars.charAt( ord >>> 18),
-            b64chars.charAt((ord >>> 12) & 63),
-            padlen >= 2 ? '=' : b64chars.charAt((ord >>> 6) & 63),
-            padlen >= 1 ? '=' : b64chars.charAt(ord & 63)
-        ];
-        return chars.join('');
+            | ((ccc.length > 2 ? ccc.charCodeAt(2) : 0));
+        return ''
+            + b64chars.charAt( ord >>> 18)
+            + b64chars.charAt((ord >>> 12) & 63)
+            + padlen >= 2 ? '=' : b64chars.charAt((ord >>> 6) & 63)
+            + padlen >= 1 ? '=' : b64chars.charAt(ord & 63)
+        ;
     };
     var btoa = global.btoa ? function(b) {
         return global.btoa(b);
@@ -81,10 +83,10 @@
             var cp = ((0x07 & cccc.charCodeAt(0)) << 18)
                 |    ((0x3f & cccc.charCodeAt(1)) << 12)
                 |    ((0x3f & cccc.charCodeAt(2)) <<  6)
-                |     (0x3f & cccc.charCodeAt(3)),
-            offset = cp - 0x10000;
-            return (fromCharCode((offset  >>> 10) + 0xD800)
-                    + fromCharCode((offset & 0x3FF) + 0xDC00));
+                |     (0x3f & cccc.charCodeAt(3));
+            var offset = cp - 0x10000;
+            return fromCharCode((offset  >>> 10) + 0xD800)
+                + fromCharCode((offset & 0x3FF) + 0xDC00);
         case 3:
             return fromCharCode(
                 ((0x0f & cccc.charCodeAt(0)) << 12)
@@ -102,13 +104,15 @@
         return b.replace(re_btou, cb_btou);
     };
     var cb_decode = function(cccc) {
-        var len = cccc.length,
-        padlen = len % 4,
-        n = (len > 0 ? b64tab[cccc.charAt(0)] << 18 : 0)
+        var len = cccc.length;
+        var padlen = len % 4;
+        var n = 0
+            | (len > 0 ? b64tab[cccc.charAt(0)] << 18 : 0)
             | (len > 1 ? b64tab[cccc.charAt(1)] << 12 : 0)
             | (len > 2 ? b64tab[cccc.charAt(2)] <<  6 : 0)
-            | (len > 3 ? b64tab[cccc.charAt(3)]       : 0),
-        chars = [
+            | (len > 3 ? b64tab[cccc.charAt(3)]       : 0)
+        ;
+        var chars = [
             fromCharCode( n >>> 16),
             fromCharCode((n >>>  8) & 0xff),
             fromCharCode( n         & 0xff)
