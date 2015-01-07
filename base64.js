851032ecd2a1c44592b39f6731cc94e105b98dce
foomin10 <foomin10@gmail.com>
2015-01-07 23:19:16 +0900

    fix conditional operators and so

--- base64.js
+++ base64.js
@@ -27,12 +27,20 @@
     var cb_utob = function(c) {
         if (c.length < 2) {
             var cc = c.charCodeAt(0);
-            return cc < 0x80 ? c
-                : cc < 0x800 ? (fromCharCode(0xc0 | (cc >>> 6))
-                                + fromCharCode(0x80 | (cc & 0x3f)))
-                : (fromCharCode(0xe0 | ((cc >>> 12) & 0x0f))
-                   + fromCharCode(0x80 | ((cc >>>  6) & 0x3f))
-                   + fromCharCode(0x80 | ( cc         & 0x3f)));
+            if (cc < 0x80) {
+                return c;
+            } else if (cc < 0x800) {
+                return ''
+                    + fromCharCode(0xc0 | (cc >>> 6))
+                    + fromCharCode(0x80 | (cc & 0x3f))
+                ;
+            } else {
+                return ''
+                    + fromCharCode(0xe0 | ((cc >>> 12) & 0x0f))
+                    + fromCharCode(0x80 | ((cc >>>  6) & 0x3f))
+                    + fromCharCode(0x80 | ( cc         & 0x3f))
+                ;
+            }
         } else {
             var cc = 0x10000
                 + (c.charCodeAt(0) - 0xD800) * 0x400
@@ -57,22 +65,26 @@
         return ''
             + b64chars.charAt( ord >>> 18)
             + b64chars.charAt((ord >>> 12) & 63)
-            + padlen >= 2 ? '=' : b64chars.charAt((ord >>> 6) & 63)
-            + padlen >= 1 ? '=' : b64chars.charAt(ord & 63)
+            +( (padlen >= 2) ? '=' : b64chars.charAt((ord >>> 6) & 63) )
+            +( (padlen >= 1) ? '=' : b64chars.charAt( ord        & 63) )
         ;
     };
-    var btoa = global.btoa ? function(b) {
-        return global.btoa(b);
-    } : function(b) {
-        return b.replace(/[\s\S]{1,3}/g, cb_encode);
-    };
+    var btoa = global.btoa
+        ? function(b) {
+            return global.btoa(b);
+        }
+        : function(b) {
+            return b.replace(/[\s\S]{1,3}/g, cb_encode);
+        }
+    ;
     var _encode = function (u) { return btoa(utob(u)) };
     var encode = function(u, urisafe) {
-        return !urisafe 
+        return !urisafe
             ? _encode(String(u))
             : _encode(String(u)).replace(/[+\/]/g, function(m0) {
                 return m0 == '+' ? '-' : '_';
-            }).replace(/=/g, '');
+            }).replace(/=/g, '')
+        ;
     };
     var encodeURI = function(u) { return encode(u, true) };
     // decoder stuff
@@ -80,23 +92,25 @@
     var cb_btou = function(cccc) {
         switch(cccc.length) {
         case 4:
-            var cp = ((0x07 & cccc.charCodeAt(0)) << 18)
-                |    ((0x3f & cccc.charCodeAt(1)) << 12)
-                |    ((0x3f & cccc.charCodeAt(2)) <<  6)
-                |     (0x3f & cccc.charCodeAt(3));
+            var cp = 0
+                | ((0x07 & cccc.charCodeAt(0)) << 18)
+                | ((0x3f & cccc.charCodeAt(1)) << 12)
+                | ((0x3f & cccc.charCodeAt(2)) <<  6)
+                |  (0x3f & cccc.charCodeAt(3))
+            ;
             var offset = cp - 0x10000;
             return fromCharCode((offset  >>> 10) + 0xD800)
-                + fromCharCode((offset & 0x3FF) + 0xDC00);
+                +  fromCharCode((offset & 0x3FF) + 0xDC00);
         case 3:
-            return fromCharCode(
-                ((0x0f & cccc.charCodeAt(0)) << 12)
-                    | ((0x3f & cccc.charCodeAt(1)) << 6)
-                    |  (0x3f & cccc.charCodeAt(2))
+            return fromCharCode(0
+                | ((0x0f & cccc.charCodeAt(0)) << 12)
+                | ((0x3f & cccc.charCodeAt(1)) <<  6)
+                |  (0x3f & cccc.charCodeAt(2))
             );
         default:
-            return  fromCharCode(
-                ((0x1f & cccc.charCodeAt(0)) << 6)
-                    |  (0x3f & cccc.charCodeAt(1))
+            return  fromCharCode(0
+                | ((0x1f & cccc.charCodeAt(0)) << 6)
+                |  (0x3f & cccc.charCodeAt(1))
             );
         }
     };
@@ -107,29 +121,32 @@
         var len = cccc.length;
         var padlen = len % 4;
         var n = 0
-            | (len > 0 ? b64tab[cccc.charAt(0)] << 18 : 0)
-            | (len > 1 ? b64tab[cccc.charAt(1)] << 12 : 0)
-            | (len > 2 ? b64tab[cccc.charAt(2)] <<  6 : 0)
-            | (len > 3 ? b64tab[cccc.charAt(3)]       : 0)
+            | ((len > 0) ? (b64tab[cccc.charAt(0)] << 18) : 0)
+            | ((len > 1) ? (b64tab[cccc.charAt(1)] << 12) : 0)
+            | ((len > 2) ? (b64tab[cccc.charAt(2)] <<  6) : 0)
+            | ((len > 3) ?  b64tab[cccc.charAt(3)]        : 0)
         ;
         var chars = [
             fromCharCode( n >>> 16),
             fromCharCode((n >>>  8) & 0xff),
-            fromCharCode( n         & 0xff)
+            fromCharCode( n         & 0xff),
         ];
         chars.length -= [0, 0, 2, 1][padlen];
         return chars.join('');
     };
-    var atob = global.atob ? function(a) {
-        return global.atob(a);
-    } : function(a){
-        return a.replace(/[\s\S]{1,4}/g, cb_decode);
-    };
+    var atob = global.atob
+        ? function(a) {
+            return global.atob(a);
+        }
+        : function(a) {
+            return a.replace(/[\s\S]{1,4}/g, cb_decode);
+        }
+    ;
     var _decode = function(a) { return btou(atob(a)) };
-    var decode = function(a){
-        return _decode(
-            String(a).replace(/[-_]/g, function(m0) { return m0 == '-' ? '+' : '/' })
-                .replace(/[^A-Za-z0-9\+\/]/g, '')
+    var decode = function(a) {
+        return _decode(String(a)
+            .replace(/[-_]/g, function(m0) { return m0 == '-' ? '+' : '/' })
+            .replace(/[^A-Za-z0-9\+\/]/g, '')
         );
     };
     var noConflict = function() {
